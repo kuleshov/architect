027366dacc5a39a98c2553a85792a4df2118ab8c
Volodymyr Kuleshov <kuleshov@stanford.edu>
2013-09-12 11:08:33 -0700

    Automatic detection of misassemblies

--- visualize.py
+++ visualize.py
@@ -28,6 +28,9 @@ def print_vertex(x):
 		# 		print_true_coordinates(w, ctg)
 	print
 
+###############################################################################
+## DETERMINE THE TRUE REFERENCE SEQUENCES THAT ARE CONTAINED IN A CONTIG
+
 def get_true_intervals(w, ctgs):
 	I = list()
 	for ctg in ctgs:
@@ -97,6 +100,9 @@ def load_genome():
 	return genome
 
 def common_overlap(i1, i2):
+	""" Find the common overlap of i1, i2. """
+	# NOTE: Doesn't work b/c we don't consider rev. compl.
+
 	# i2 -> i1?
 	for i1_start_in_i2 in xrange(len(i2)):
 		# l = len(i2) - i1_start_in_i2
@@ -111,6 +117,35 @@ def common_overlap(i1, i2):
 		if i1[i2_start_in_i1:] == i2[:l]:
 			return i2[:l]
 
+def get_containments(v, true_intervals, genome):
+	"""
+	Returns a list of that descripbes how true_intervals are contained 
+	in the assembled sequence of v.
+	A containment is a tuple (start_pos, end_pos, orientation).
+	"""
+	containments = list()
+	assembly = v.seq
+	for i in true_intervals:
+		# print i, i[2]-i[1]+1,
+		true_region = genome[i[0]][i[1]:i[2]+1]
+
+		# try same strand:
+		for j in xrange(len(assembly)):
+			if true_region == assembly[j:j+len(true_region)]:
+				containments.append((j, j+len(true_region)-1, 0))
+				break
+
+		# try opposite strands:
+		true_region_opp = reverse_complement(true_region)
+		for j in xrange(len(assembly)):
+			if true_region_opp == assembly[j:j+len(true_region_opp)]:
+				containments.append((j, j+len(true_region_opp)-1, 1))
+				break
+
+		# containments.append((-1, -1, -1))
+
+	return containments
+
 def print_containment(v, true_intervals, genome):
 	# if len(v.seq) > 250: return
 	print '-----------------------------------------------------------'
@@ -118,33 +153,9 @@ def print_containment(v, true_intervals, genome):
 	# print v.seq
 	print 'VERIFYING INTERVALS:', ', '.join([str(i) for i in true_intervals])
 
-	assembly = v.seq
-	for i in true_intervals:
-		print i, i[2]-i[1]+1,
-		true_region = genome[i[0]][i[1]:i[2]+1]
-
-		# try same strang:
-		for j in xrange(len(assembly)):
-			if true_region == assembly[j:j+len(true_region)]:
-				print j, j+len(true_region),
-				break
-
-		# try opposite strands:
-		true_region_opp = reverse_complement(true_region)
-		for j in xrange(len(assembly)):
-			if true_region_opp == assembly[j:j+len(true_region_opp)]:
-				print j, j+len(true_region_opp),
-				break
-
-		# print true_region
-		print
-
-def visualize_assembly(v, I, genome):
-	print v.seq
-	for i, ctg in enumerate(v.metadata['contigs']):
-		print ctg, v.metadata['contig_starts'][ctg], v.metadata['contig_ends'][ctg]
-		print genome[I[i][0]][I[i][1]:I[i][2]+1]
-
+	containments = get_containments(v, true_intervals, genome)
+	for c, intv in zip(containments, true_intervals):
+		print intv, intv[2]-intv[1]+1, c
 
 def examine_repeats(intervals, genome):
 	print '-----------------------------------------------------------'
@@ -160,11 +171,42 @@ def examine_repeats(intervals, genome):
 	if len(intervals) == 2:
 		a = intervals[0]
 		b = intervals[1]
+		# need to consider rev. compl. in this function:
 		o = common_overlap(genome[a[0]][a[1]:a[2]+1], genome[b[0]][b[1]:b[2]+1])
 		if o:
 			print o
 		else:
 			print "overlap not found"
+
+def is_misassembly(v, true_intervals, genome):
+	"""
+	If a true genomic region does not touch the 200bp-long edges of a contig,
+	we consider that it is a misassembly.
+	"""
+	L = len(v.seq)
+	containments = get_containments(v, true_intervals, genome)
+	for c, intv in zip(containments, true_intervals):
+		if c[0] > 200 or c[1] < L - 200:
+			return True
+
+	return False
+
+###############################################################################
+## VISUALIZE SUB-COMPONENTS OF THE GRAPH
+
+def visualize_assembly(v, I, genome):
+	print v.seq
+	for i, ctg in enumerate(v.metadata['contigs']):
+		print ctg, v.metadata['contig_starts'][ctg], v.metadata['contig_ends'][ctg]
+		print genome[I[i][0]][I[i][1]:I[i][2]+1]
+
+def visualize_connection(e):
+	v1, v2 = e.v1, e.v2
+	print e.ovl_start[v1], e.ovl_end[v1], e.ovl_start[v2], e.ovl_end[v2], e.connection[v1], e.connection[v2], e.v2_orientation
+	print v1.seq
+	print v2.seq
+	print v1.seq[e.ovl_start[v1]:e.ovl_end[v1]+1]
+	print v2.seq[e.ovl_start[v2]:e.ovl_end[v2]+1]
 
 ###############################################################################
 ## SAVE TO DOT
@@ -210,13 +252,16 @@ def to_graphviz_dot_with_intervals(g, dot_file):
 			if len(I) > 1:
 				color = "red"
 				# examine_repeats(I, genome)
-				print_containment(v, I, genome)
+				if is_misassembly(v, I, genome):
+					print_containment(v, I, genome)
 			else:
 				color = "black"
 
 			dot.write('%d [label = "%s", color="%s"]\n' % (v.id_, ','.join([str(i) for i in I]), color))
 		for e in g.edges:
 			v1, v2 = e.v1, e.v2
+			# if v1.id_ == 3706567 and v2.id_ == 3712515:
+			# 	visualize_connection(e)
 			dot.write('"%d" -> "%d" [label= "%d %d %d %d %s%s %d"]\n' % (v1.id_, v2.id_, e.ovl_start[v1], e.ovl_end[v1], e.ovl_start[v2], e.ovl_end[v2], e.connection[v1], e.connection[v2], e.v2_orientation))
 
 		dot.write('}\n')
