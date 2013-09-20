b3d6fd25894bd655c1a5e902fe994ea081a07bfa
Volodymyr Kuleshov <kuleshov@stanford.edu>
2013-09-19 22:17:35 -0700

    Move stats function

--- graph_stats.py
+++ graph_stats.py
@@ -1,5 +1,12 @@
 from libkuleshov.stats import n50
 
+def print_stats(g):
+	print "Vertices:", len(g.vertices)
+	print "Edges:", len(g.edges)
+	print "Connected components:", g.count_connected_components()
+	print "N50:", graph_n50(g)
+	print "Average contig length:", graph_avg(g)
+
 def graph_n50(g):
 	return n50([len(v) for v in g.vertices])
 
